c47b3863d06d66bafcb3d72dbf6d98a1f66514bb
Michael <wtfdic@hotmail.com>
2020-11-23 19:26:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,12 @@
 def badge_maker(name)
   return "Hello my name is #{name}."
 end
+
+def batch_badge_creator(speakers)
+  badge_message = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
+  end
+  badge_messages
+end
