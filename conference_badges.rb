9161f0f023682ebf5b5a561f71a7549d2888636b
Michael <wtfdic@hotmail.com>
2020-11-23 19:29:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,24 @@ def batch_badge_creator(speakers)
   end
   badge_messages
 end
+
+def assign_rooms(speakers)
+  room_number = 1
+  room_message + []
+  speakers.each do |speaker|
+    room_messages = "Helo #{speaker}! You'll be assisgned to room #{room_number}!"
+    room_number += 1
+  end
+  room_messages
+end
+
+def printer(speakers)
+  badge_messages = batch_badge_creator(speakers)
+  badge_messages.each do |message|
+    puts message
+  end
+  room_messages = assisgn_rooms(speakers)
+  rooms_messages.each do |message|
+    puts message
+  end
+end
